95224f4caf4e8f949b09cccaaa576cb01e8c4a37
Marcel Benzinger <benzinger.marcel@gmail.com>
2023-08-07 17:23:24 +0200

    finished the project for first release

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function fadeOutOnScroll() {
-    const elements = document.querySelectorAll("#main");
-
-    function handleScroll() {
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      // Set the opacity of each element based on the scroll position
-      elements.forEach((element) => {
-        const scrollOpacity = 1 - scrollTop / 850;
-        // Ensure opacity is between 0 and 1
-        const opacity = Math.min(Math.max(scrollOpacity, 0), 1);
-        element.style.opacity = opacity.toFixed(2);
-      });
-    }
-
-    // Attach the handleScroll function to the scroll event
-    window.addEventListener("scroll", handleScroll);
-
-    // Initial call to set the opacity based on the current scroll position
-    handleScroll();
-  }
-
-  // Call the function to initiate the effect when the page loads
-  fadeOutOnScroll();
-
-  // add a div to get to home when scrolled down in the right corner at the bottom
+// HOME BUTTON ----------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", () => {
+  const headerHeight = document.getElementById("main").offsetHeight;
 
   function scrollFunction() {
     const homeBtn = document.getElementById("home-btn");
     const scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-    if (scrollPosition > 500) {
-      homeBtn.classList.add("hide");
+    if (scrollPosition > headerHeight) {
+      homeBtn.classList.add("show");
     } else {
-      homeBtn.classList.remove("hide");
+      homeBtn.classList.remove("show");
     }
   }
 
   window.addEventListener("scroll", scrollFunction);
 });
 
-function toggleMenu(props) {
+// ----------------------------------------------------------------------------
+
+// MOBILE MENU TOGGLE ---------------------------------------------------------
+
+function toggleMenu() {
   document.getElementById("nav-menu-mobile").classList.toggle("change");
-  document.getElementById("nav-links-mobile").classList.toggle("show");
+  document.getElementById("nav-links-mobile").classList.toggle("toggle");
 
   // Close the menu when the user clicks a link
   const links = document.querySelectorAll("#nav-links-mobile a");
   links.forEach((link) => {
     link.addEventListener("click", () => {
-      document.getElementById("nav-links-mobile").classList.remove("show");
+      document.getElementById("nav-links-mobile").classList.remove("toggle");
       document.getElementById("nav-menu-mobile").classList.remove("change");
     });
   });
@@ -59,10 +40,12 @@ function toggleMenu(props) {
       targetElement.closest("#nav-links-mobile") &&
       !targetElement.closest("#nav-menu-mobile")
     ) {
-      document.getElementById("nav-links-mobile").classList.remove("show");
+      document.getElementById("nav-links-mobile").classList.remove("toggle");
       document.getElementById("nav-menu-mobile").classList.remove("change");
       // Also reset the class "change" if needed
       // props.classList.remove("change");
     }
   });
 }
+
+// ----------------------------------------------------------------------------
